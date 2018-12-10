bb6c9e3b99d575dc6d2882943b2b4c395ffc9bb6
Sameh Mahmoud <samehmahmoud2k@gmail.com>
2018-12-10 12:52:58 +0200

    creating media Class

---
+++ entertainmentCenter.py
@@ -0,0 +1,7 @@
+from movies_project import media
+
+toystory = media.Movie("toystory",
+                       "a story about a boy ...",
+                       "image",
+                       "youtube trial")
+print(toystory.title)
---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self,movie_title,movie_storyline,poster_image,trial_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trial_youtube
