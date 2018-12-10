28ee2d29fba8eb3412a891e5a35ee49119fb9741
Sameh Mahmoud <samehmahmoud2k@gmail.com>
2018-12-10 14:09:23 +0200

    adding multiple instances and creating Movie Website with the help of fresh_tomatoes.py

--- entertainmentCenter.py
+++ entertainmentCenter.py
@@ -1,7 +1,30 @@
+from movies_project import fresh_tomatoes
 from movies_project import media
 
 toystory = media.Movie("toystory",
                        "a story about a boy ...",
-                       "image",
-                       "youtube trial")
-print(toystory.title)
+                       "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+                       "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                     "a file about ,,",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+frances_ha = media.Movie("Frances HA",
+                         "a film about two girls living ..",
+                         "https://upload.wikimedia.org/wikipedia/en/6/69/Frances_Ha_poster.png",
+                         "https://www.youtube.com/watch?v=y9YKHRQkf7k")
+school_of_rock = media.Movie("school of rock",
+                             "a movie about bla bla ",
+                             "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+midnight_in_paris = media.Movie("midnight_in_paris",
+                                "a night in paris ..",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+#print(toystory.title)
+#print(avatar.storyline)
+#avatar.Show_Trailer()
+my_moveis =[toystory,avatar,frances_ha,school_of_rock,midnight_in_paris]
+fresh_tomatoes.open_movies_page(my_moveis)
